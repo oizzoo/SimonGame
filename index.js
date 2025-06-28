14290c3f298690c38a0938c5460a992c677ba13e
oizzoo <108180322+oizzoo@users.noreply.github.com>
2025-06-28 18:05:34 +0200

    Add core Simon game mechanics and user input validation
    - Generate random color sequence for each level
    - Handle button clicks and animate presses
    - Check user input against game pattern
    - Update level display dynamically
    - Play sounds with adjustable volume
    - Provide visual and audio feedback on mistakes

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var gamePattern = [];
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var currentVolume = 0.3;
+var level = 0;
+var started = false;
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour);
 
-     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+    level++;
+    $("#level-title").text(`Level ${level}`); 
+
     makeSound(randomChosenColour);
     return randomChosenColour
 }
 
+//detect when any of the buttons are clicked
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    makeSound(userChosenColour)
+    animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length - 1)
+})
+
+
+//create sound effect
 function makeSound(color) {
     var sound;
 
@@ -34,7 +52,7 @@ function makeSound(color) {
             return;
     }
 
-    sound.volume = currentVolume; // <--- to jest kluczowe!
+    sound.volume = currentVolume;
     sound.play();
 }
 
@@ -42,3 +60,50 @@ function makeSound(color) {
 $("#volumeRange").on("input", function () {
     currentVolume = parseFloat(this.value);
 });
+
+// Animate clicked colours
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+//Detect ONLY first key push
+
+$(window).one("keydown", function() {
+  if (!started) {
+      started = true;
+      nextSequence();
+      $("#level-title").text("Level " + level);
+  }
+});
+
+// checking answer
+
+function checkAnswer(currentLevel) {
+   
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success")
+    } else {
+       var wrongSound = new Audio("sounds/wrong.mp3");
+       wrongSound.volume = currentVolume;
+       wrongSound.play()
+
+       $("body").addClass("game-over");
+
+       setTimeout(function() {
+         $("body").removeClass("game-over");
+       }, 300);
+    }
+
+    if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function() {
+            nextSequence()
+            userClickedPattern = [];
+        }, 1000);
+    }
+
+}
+
